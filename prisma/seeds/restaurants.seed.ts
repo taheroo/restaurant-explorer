27940670390895386724bc932ec5a588b9d601ad
Ayadi Tahar <tahar@saveyourwardrobe.com>
2023-06-09 16:56:19 +0100

    Updated Restaurant seeds images

--- prisma/seeds/restaurants.seed.ts
+++ prisma/seeds/restaurants.seed.ts
@@ -1,40 +1,44 @@
 export default function getRestaurants() {
-
-  return [
-    {
-      name: "Restaurant Marjellchen",
-      description: `Cozy eatery with wooden dado & paneling, pictures on the walls & serving dishes from East Prussia.`,
-      cuisine: 'German',
-      location: 'Berlin',
-      imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipP5lJMfIOoiMsUWDQgi_2kWVJrgNjbeXMdh0KKr=w325-h218-n-k-no'
-    },
-    {
-      name: "Ratskeller München",
-      description: `Large cellar restaurant dating from the 19th century, with regional dishes & classic decor.`,
-      cuisine: 'Bavarian',
-      location: 'Berlin',
-      imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipODVfz0bOL8Byk6snnvBgWLpsZNiZbupl3dNVQj=s1360-w1360-h1020'
-    },
-    {
-      name: "Restaurant Atelier Gourmet",
-      description: `Inventive modern French & Mediterranean dishes, multi-course menus, paired wines & champagne.`,
-      cuisine: 'French',
-      location: 'Berlin',
-      imageUrl: "https://lh3.googleusercontent.com/p/AF1QipMP7sIyPr-mBNkjtFzJAK41VnfOwNDPMqMjfcGh=s1360-w1360-h1020"
-    },
-    {
-      name: "Zur Haxe",
-      description: `Waiting staff in dirndls & lederhosen serve meaty dishes like pork knuckle at this Bavarian stop.`,
-      cuisine: 'German',
-      location: 'Berlin',
-      imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipOA8Mqw_rU2oJjICX-JzHyX0lXhn37KE2X8FxCH=s1360-w1360-h1020'
-    },
-    {
-      name: "Steinheil 16",
-      description: `Casual restaurant specializing in schnitzel plates & fried sides, plus beer, wine & cocktails.`,
-      cuisine: 'German',
-      location: 'Berlin',
-      imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipM-YdQhnTOeVOOZoS1qRmWi7mIvAgXctWWf9P8_=w325-h218-n-k-no'
-    }
-  ];
+	return [
+		{
+			name: 'Restaurant Marjellchen',
+			description: `Cozy eatery with wooden dado & paneling, pictures on the walls & serving dishes from East Prussia.`,
+			cuisine: 'German',
+			location: 'Berlin',
+			imageUrl:
+				'https://res.cloudinary.com/dc9b3xgwe/image/upload/v1686305404/nr9yzdbrikd6e1zwfssw.jpg',
+		},
+		{
+			name: 'Ratskeller München',
+			description: `Large cellar restaurant dating from the 19th century, with regional dishes & classic decor.`,
+			cuisine: 'Bavarian',
+			location: 'Berlin',
+			imageUrl:
+				'https://res.cloudinary.com/dc9b3xgwe/image/upload/v1686305192/jwz2fygvabip5moxnji4.jpg',
+		},
+		{
+			name: 'Restaurant Atelier Gourmet',
+			description: `Inventive modern French & Mediterranean dishes, multi-course menus, paired wines & champagne.`,
+			cuisine: 'French',
+			location: 'Berlin',
+			imageUrl:
+				'https://res.cloudinary.com/dc9b3xgwe/image/upload/v1686305636/duglgncrgnyycpzn72dt.jpg',
+		},
+		{
+			name: 'Zur Haxe',
+			description: `Waiting staff in dirndls & lederhosen serve meaty dishes like pork knuckle at this Bavarian stop.`,
+			cuisine: 'German',
+			location: 'Berlin',
+			imageUrl:
+				'https://res.cloudinary.com/dc9b3xgwe/image/upload/v1686305332/pnyca5nuihrbriwwmr4d.jpg',
+		},
+		{
+			name: 'Steinheil 16',
+			description: `Casual restaurant specializing in schnitzel plates & fried sides, plus beer, wine & cocktails.`,
+			cuisine: 'German',
+			location: 'Berlin',
+			imageUrl:
+				'https://res.cloudinary.com/dc9b3xgwe/image/upload/v1686305579/wwh2agiqdwzisdgfques.jpg',
+		},
+	];
 }
